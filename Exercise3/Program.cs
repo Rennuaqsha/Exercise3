31b1f2d28bfff32e75cc610a7d91c17ebfc2db2c
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-21 16:37:50 +0800

    create class node

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Xml;
+
+namespace Exercise_linked_list_A
+{
+    class node
+    {
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public node next;
+    }
+}
