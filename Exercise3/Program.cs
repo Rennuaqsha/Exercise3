e5b288b722f2e651760450348a6df8780fa3a2f6
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-21 16:52:27 +0800

    create method class search

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -18,5 +18,19 @@ namespace Exercise_linked_list_A
         {
             Last = null;
         }
+        public bool Search(int rollNo, ref previous, ref node current)
+        /*Searches for the specified node*/
+        {
+            for (previous = current = Last.next; current != Last; previous = current,
+                 current = current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true); /*return true if the node is found*/
+            }
+            if (rollNo == Last.rollNumber)/*if the node is present at the end*/
+                return true;
+            else
+                return (false);/*return false if the node is not found*/ 
+        }
     }
 }
